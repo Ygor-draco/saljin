fe808952f9403908a1703c7f1ddaac3907b3737a
Draco <ygorvbarbosal@gmail.com>
2021-01-25 23:34:50 -0300

    testando

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ export default function home(){
             <h1>Rola iniciativa ai cambada</h1>
             <h4>vai todo mundo morrer</h4>
             <p>Mussum Ipsum, cacilds vidis litro abertis. Quem manda na minha terra sou euzis! Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Nec orci ornare consequat.<br /> Praesent lacinia ultrices consectetur. Sed non ipsum felis. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
-            
+            <button>Testando essa bagaça?</button>
+            <h6>Tem certeza que vai dar certo?</h6>
         </div>
     )
 }
