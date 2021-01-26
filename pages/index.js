f11bf294078aeea4b3ff9837fa6d70eaaffa3cf2
Draco <ygorvbarbosal@gmail.com>
2021-01-25 23:56:08 -0300

    testebom

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function home(){
             <p>Mussum Ipsum, cacilds vidis litro abertis. Quem manda na minha terra sou euzis! Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Nec orci ornare consequat.<br /> Praesent lacinia ultrices consectetur. Sed non ipsum felis. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
             <a href="http://google.com.br"><button>Testando essa bagaça?</button></a>
             <h6>Tem certeza que vai dar certo?</h6>
+            <img width="80%" src="img/region.jpg" />
         </div>
     )
 }
